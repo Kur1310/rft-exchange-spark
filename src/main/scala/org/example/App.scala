e8a0c3d4c112036255ff5eb00ad0503464ce7497
keyurshah <keyurshah.1310@gmail.com>
2022-01-06 19:31:13 +0530

    commit 1

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -17,12 +17,7 @@ object App extends App {
   val df = spark.read.option("delimiter",";").format("org.apache.spark.sql.execution.datasources.v2.csv.CSVDataSourceV2").load("gs://ac-rftexchange-qa-project-dataproc-staging/SparkJar/data.csv")
 
 
-  df.printSchema()
-  val rdd = spark.sparkContext.parallelize(data)
-  val dfFromRDD2 = spark.createDataFrame(rdd).toDF(columns:_*)
-
-
-  dfFromRDD2.printSchema()
+  df.count()
 
 
 
