51cc45c0e623e6f08806f92d2a0815e1fb1c3280
punitwalawalkar <wpunit13@gmail.com>
2022-01-07 17:51:49 +0530

    write to bucket changed the format

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -31,7 +31,7 @@ object App extends App {
   println(ca.count())
   println(pty.count())
 
-  ca.write.format("org.apache.spark.sql.execution.datasources.v2.parquet.ParquetDataSourceV2").save("gs://ac-rftexchange-qa-project-dataproc-staging/Output/customer.parquet")
+  ca.repartition(10).write.format("org.apache.spark.sql.execution.datasources.v2.json.JsonDataSourceV2").save("gs://ac-rftexchange-qa-project-dataproc-staging/Output/customer.json")
 
 
 }
