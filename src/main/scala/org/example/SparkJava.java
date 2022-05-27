0703a9f42d7d7e2bc1cc5ed238c3840bfe94b830
keyurshah <keyurshah.1310@gmail.com>
2022-05-27 16:05:06 +0530

    Added Logger

--- src/main/scala/org/example/SparkJava.java
+++ src/main/scala/org/example/SparkJava.java
@@ -3,17 +3,22 @@ package org.example;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SparkJava {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SparkJava.class);
     public static void main(String[] args) {
 
         String master ="local[*]";
 
         SparkSession spark =SparkSession.builder().appName("SparkJava").master(master).getOrCreate();
-
+        LOGGER.info("Reading JSON file --Start");
         Dataset<Row> ca = spark.read().option("multiline",true).
                 format("org.apache.spark.sql.execution.datasources.json.JsonFileFormat").load("gs://rtf-xchnage-spark-test/SparkJar/ca.json");
+        LOGGER.info("Reading JSON file --END");
         ca.printSchema();
+        LOGGER.info("Show JSON Dataset file");
         ca.show();
 
     }
